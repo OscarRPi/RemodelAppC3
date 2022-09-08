d92aef983be46ed2d71ed3db4fdaae6a1ac5f9de
thencabito <orpinedal@unal.edu.co>
2022-09-09 01:23:29 +0200

    Primer version de la pagina WEB uniendo back con front

---
+++ RemodelApp/urls.py
@@ -0,0 +1,35 @@
+"""RemodelApp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+#from products.views import ProductListView
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('', ProductListView.as_view(), name="index"),
+    path('', views.index, name="index"),
+    path('users/login', views.login_view, name="login"),
+    path('users/logout', views.logout_view, name="logout"),
+    #path('products/', include('products.urls')),
+    #path('dashboard/', include('dashboard.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
